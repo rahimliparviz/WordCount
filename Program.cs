f1e9d0832fbfd5079191fb0ebaf25a47bb31f7ff
rahimliparviz <rahimliparviz@gmail.com>
2022-10-10 13:05:48 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordCounter = new WordCounter();
+            wordCounter.take("aa");
+            wordCounter.take("aa");
+            wordCounter.take("bb");
+            Console.WriteLine(wordCounter.count("aa"));
+            Console.WriteLine(wordCounter.count("bb"));
+            Console.WriteLine(wordCounter.count("cc"));
+
+        }
+    }
+}
---
+++ WordCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+     class WordCounter
+    {
+        private readonly object _locker = new object();
+
+        private Dictionary<string, int> _wordCountDictionary  = new Dictionary<string, int>();
+        public void take(String word){
+            lock (_locker)
+            {
+
+                if (_wordCountDictionary.ContainsKey(word))
+                {
+                    _wordCountDictionary[word] += 1;
+                }
+                else
+                {
+                    _wordCountDictionary[word] = 1;
+                }
+            }
+        }
+
+        public int count(String word) {
+            if (_wordCountDictionary.ContainsKey(word))
+            {
+                return _wordCountDictionary[word];
+
+            }
+
+            return 0;
+        }
+
+    }
+}
